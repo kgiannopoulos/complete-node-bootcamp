ce671cb3dab2a1f301544c75626e683520db0d83
kgiannopoulos <kg4756@ou.ac.uk>
2019-07-26 15:57:29 +0300

    from work

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//
+// //Blocking Synchronous way
+//
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const texOut = `This is what we know about the avocado: ${textIn}\n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', texOut);
+// console.log('File written!');
+//
+// //non blocking asycnhrnous
+
+//
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('Error');
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+//
+//                 fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err  => {
+//                     console.log('file has been written ;)');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file');
+
+/////////////////////////SERVER////////////////////////////
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject  = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    } else if (pathName === '/product') {
+        res.end('This is the product');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+    });
+
+
+server.listen(8080, '127.0.0.1', () => {
+
+    console.log('Listening to requests on port 8080');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
